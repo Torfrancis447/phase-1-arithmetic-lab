1680bb52fc99c9a9d2176873c2da42a615922f7b
tor <torfrancis447@gmail.com>
2022-10-31 21:55:04 -0400

    commit tree ce11ef7aed935a66dcd7f94735e0ca4a4362d51d

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = 2 * 31
+let random = Math.round(Math.random() * 10)
+let num3 = 10
+let num4 = 6
+let mod = (num3 % num4)
+let max = Math.max(1, 3 ,4, 7, 8, 3, 14, 20, 9)
